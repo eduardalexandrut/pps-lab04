0fc74556e5b7fbbadca8d45c0f5ef62cdd3d7910
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2026-04-15 22:23:22 +0200

    feat: Complete task 1

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -18,13 +18,23 @@ object Ex1ComplexNumbers:
       def asString(): String
 
   object BasicComplexADT extends ComplexADT:
-
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    case class ComplexNumber(rePart: Double, imPart: Double)
+
+    type Complex = ComplexNumber
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.rePart
+      def im(): Double = complex.imPart
+      def sum(other: Complex): Complex = ComplexNumber(complex.rePart + other.re(), complex.imPart + other.imPart)
+      def subtract(other: Complex): Complex = ComplexNumber(complex.rePart - other.re(), complex.imPart - other.imPart)
+      def asString(): String =
+        val r = complex.rePart
+        val i = complex.imPart
+
+        (r, i) match
+          case (0, 0) => "0.0"
+          case (0, _) => s"${complex.imPart}i"
+          case (_, 0) => s"${complex.rePart}"
+          case (_, pos) if pos > 0 => s"${complex.rePart} + ${pos}i"
+          case (_, neg) if neg < 0 => s"${complex.rePart} - ${neg.abs}i"
